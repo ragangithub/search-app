bdab898da628ab560f072a3c8acf4509949bb7e9
Ragan <raganalemu3@gmail.com>
2024-01-20 15:28:50 +0300

    Add seed data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    validates :ip_address, presence: true
+    has_many :searches
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+articles = Article.create([
+  { name: "Exploring AI Advancements" },
+  { name: "Sustainable Living Solutions" },
+  { name: "Unveiling Cutting-Edge Technologies" },
+  { name: "Tech Innovations Shaping the Future" },
+  { name: "Revolutionizing Healthcare with Science" },
+  { name: "The Digital Economy Boom" },
+  { name: "Reshaping Business Dynamics" },
+  { name: "Urbanization and Modernization" },
+  { name: "Towards a Greener Tomorrow" },
+  { name: "Innovative Breakthroughs Unleashed" },
+  { name: "Securing Data in the Digital Age" },
+  { name: "Navigating Ethical Challenges" },
+  { name: "Transforming Manufacturing Processes" },
+  { name: "Industries in the Era of Transformation" },
+  { name: "The Wellness Revolution" },
+  { name: "Emerging Realities in the Digital World" },
+  { name: "Building a Connected Society" },
+  { name: "Ensuring Information Security" },
+  { name: "The Impact of Sustainable Savings" },
+  { name: "The Next Generation of Technology" }
+])
