7bacc3b2ff57718a7b0ccc8a6c9da47825fe2b51
Ragan <raganalemu3@gmail.com>
2024-01-20 19:01:25 +0300

    Add real time query

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    before_action :set_current_user
+
+    def current_user
+      @current_user ||= User.find_or_create_by(ip_address: request.remote_ip)
+    end
+  
+    private
+  
+    def set_current_user
+      @current_user = current_user
+    end
 end
